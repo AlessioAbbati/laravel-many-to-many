07bb71fd7501b8f9956e4e72c6687afaea902810
AlessioAbbati <alex_117@live.it>
2023-07-11 17:40:12 +0200

    mod models and add techtableseeder

--- database/migrations/2023_07_11_153144_create_project_technology_table.php
+++ database/migrations/2023_07_11_153144_create_project_technology_table.php
@@ -6,24 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+    
     public function up()
     {
         Schema::create('project_technology', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('project_id'); 
+            $table->unsignedBigInteger('technology_id'); 
+            
+            
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('technology_id')->references('id')->on('technologies');
+            
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+    
+    
     public function down()
     {
         Schema::dropIfExists('project_technology');
