0915e131f22b28ade6f5c4d7f9ec06623135d6a5
AlessioAbbati <alex_117@live.it>
2023-07-11 18:39:04 +0200

    mod show

--- resources/views/Admin/projects/show.blade.php
+++ resources/views/Admin/projects/show.blade.php
@@ -10,7 +10,7 @@
                 <th scope="col">Last Update</th>
                 <th scope="col">Collaborators</th>
                 <th scope="col">Description</th>
-                <th scope="col">Languages</th>
+                <th scope="col">Technologies</th>
                 <th scope="col">Link</th>
                 <th scope="col">Actions</th>
             </tr>
@@ -23,7 +23,7 @@
                 <td>{{ $project->last_update }}</td>
                 <td>{{ $project->collaborators }}</td>
                 <td>{{ $project->description }}</td>
-                <td>{{ $project->languages }}</td>
+                <td>{{ implode(', ', $project->technologies->pluck('name')->all()) }}</td>
                 <td><a href="{{ $project->link_github }}">Link</a></td>
 
                 <td>
